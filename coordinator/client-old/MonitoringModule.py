19bb722b107376f51e8ba57d98006faae851e11c
emnavarro02 <emnavarro02@gmail.com>
2020-11-21 21:11:53 +0100

    strutcture amendments

--- coordinator/client-old/MonitoringModule.py
+++
@@ -1,5 +0,0 @@
-class MonitoringModule:
-    def funcname(self, deviceId, alertState, userState):
-        self.deviceId = deviceId
-        self.alertState = alertState
-        self.userState = userState
